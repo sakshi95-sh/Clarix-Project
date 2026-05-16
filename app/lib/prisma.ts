8903ccef4de79075b20ddb967063f14d1f67a66e
Sakshi Sharma <sakshi02feb@gmail.com>
2026-05-16 10:37:05 -0400

    fix prisma import path

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,10 +1,8 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
-const connectionString = process.env.DATABASE_URL;
-
+const connectionString = process.env.DATABASE_URL!;
 const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString }),
+  adapter: new PrismaPg(connectionString),
 });
 
 export { prisma };
