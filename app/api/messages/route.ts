f848259fa006cbe8059a9e00d199e36079d7375d
Sakshi Sharma <sakshi02feb@gmail.com>
2026-09-14 21:22:56 -0400

    Week 1: v5

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -3,7 +3,6 @@ import { verifyAuth } from "@/app/lib/auth";
 
 export async function GET(request: Request) {
   const userId = await verifyAuth();
-      console.log("USER ID------",userId)
 
   if (userId) {
     const { searchParams } = new URL(request.url);
